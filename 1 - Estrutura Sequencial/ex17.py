ad07b7654c4a78421be96b6adfd7ae0cf6ffdd8a
tengan <tenganowner@hotmail.com>
2020-03-10 19:26:28 -0300

    commit 1003

--- 1 - Estrutura Sequencial/ex17.py
+++ 1 - Estrutura Sequencial/ex17.py
@@ -12,3 +12,35 @@
 #-misturar latas e galoes, de forma que o preco seja o menor. Acrescente 10% de folga e
 #sempre arredonde os valores para cima, isto e, considere latas cheias.
 
+x = input('Quantos metros quadrados voce ira pintar? :')
+
+#lata = 18 litros = 6*18 = 108 m2
+#galao = 3.6 litros = 6*3.6 = 21,6 m2
+
+qtdLata = x/108
+restoLata = x%108
+if (qtdLata == 0 and x >= 1  or restoLata>0): #controle Lata
+    qtdLata += 1
+
+qtdGalao =int(x/21.6)
+restoGalao = x%21.6
+
+if (qtdGalao == 0 and x >= 1  or restoGalao>0): #controle Galao
+    qtdGalao += 1
+
+print('Usando apenas Latas de 18 litros, vc ira precisar de %s lata(s), e custara R$ %s' %(qtdLata, qtdLata*80))
+print('Usando apenas Galao de 3.6 litros, vc ira precisar de %s galao(oes), e custara R$ %s' %(qtdGalao, qtdGalao*25))
+
+##menor custo com mistura
+##Saber quando eh mais viavel um do que o outro
+
+## 4 galoes(R$ 100 e 86,4 m2 ) sai mais caro que 1 lata(R$ 80 e 108 m2)
+if ( x>(21.6*3) and qtdLata<1):
+
+
+
+
+####terminar
+
+
+print('Usando Galoes e Latas, vc ira precisar de %s Galoes e %s Latas, e isso custara R$ %s' %() )
--- 1 - Estrutura Sequencial/ex18.py
+++ 1 - Estrutura Sequencial/ex18.py
@@ -1,3 +1,10 @@
 #Faca um programa que peca o tamanho de um arquivo para download (em MB) e a velocidade de
 #um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo
 #usando este link (em minutos).
+
+tam = input('Qual tamanho do arquivo em MB para baixar? ')
+vel = input('Qual velocidade da internet em Mbps ? ')
+tam = int(tam)
+vel = int(vel)
+
+print('O arquivo sera baixado em %s min aproximadamente'  %( tam/(vel*60) ))
